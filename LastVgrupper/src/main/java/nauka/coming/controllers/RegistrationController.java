9a35adbe4ef6a8aec182fe4d7b7038b510c8ee37
Khamit <khamitamantaev@gmail.com>
2020-05-29 22:12:39 +0500

    начальные приготовления и все будет норм дальше, попробую доделать Vgrupper под форум

---
+++ LastVgrupper/src/main/java/nauka/coming/controllers/RegistrationController.java
@@ -0,0 +1,18 @@
+package nauka.coming.controllers;
+
+import nauka.coming.dto.UserDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.context.request.WebRequest;
+
+@Controller
+public class RegistrationController {
+
+    @GetMapping("/user/registration")
+    public String showRegistrationForm(WebRequest request, Model model) {
+        UserDto userDto = new UserDto();
+        model.addAttribute("user", userDto);
+        return "registration";
+    }
+}
